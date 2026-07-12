4bc3eb422554150770a3fa765cb4094750404049
Julliana Zate <jullianazate@Jullianas-MacBook-Air-2.local>
2026-07-12 20:40:16 +0800

    Fixed app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = os.getenv("SECRET_KEY")
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        port=os.getenv("DB_PORT"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
